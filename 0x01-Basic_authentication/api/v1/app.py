7fb208c343c7359eb93a62d663e24b78de66a538
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-04-17 16:42:06 +0300

    Basic authentication 01

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 """
-Main module for the API
+Route module for the API
 """
-from flask import Flask, jsonify
+from os import getenv
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
@@ -16,6 +28,6 @@ def unauthorized(error):
 
 
 if __name__ == "__main__":
-    host = os.getenv('API_HOST', '0.0.0.0')
-    port = int(os.getenv('API_PORT', '5000'))
-    app.run(host=host, port=port, threaded=True)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
