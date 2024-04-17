4e813cc7f7bb9badb8855b40cfd6bc3fc43caec6
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-04-17 16:44:52 +0300

    Basic authentication 01 - route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'])
+def unauthorized():
+    """Route to trigger unauthorized error"""
+    abort(401)
