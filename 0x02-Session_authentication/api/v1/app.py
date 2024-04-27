00218a40d33e7b37ae50d669c9c32d10b6671cae
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-04-27 20:55:47 +0300

    session auth 6

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -44,7 +44,7 @@ def before_request():
         '/api/v1/status',
         '/api/v1/unauthorized',
         '/api/v1/forbidden',
-        '/api/v1/auth_session/login/'
+        '/api/v1/auth_session/login'
     ]
     if request.path in excluded_paths:
         return
