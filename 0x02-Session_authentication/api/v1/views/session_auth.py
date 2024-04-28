068ad8c9fde79f4f70113c49ace0f364dbdb2624
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-04-28 09:41:02 +0300

    session auth 9

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,11 +8,6 @@ from api.v1.views import app_views
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-@app_views.route(
-    '/auth_session/login/',
-    methods=['POST'],
-    strict_slashes=False
-)
 def login() -> str:
     """ Handle user login """
     email = request.form.get('email')
